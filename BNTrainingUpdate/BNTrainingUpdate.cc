83e4b1ddb5f016e8fb976c2e08e0fdef1a5431e2
qili93 <qili93@qq.com>
2021-09-11 01:05:52 +0800

    update bn to 3D input

--- BNTrainingUpdate/BNTrainingUpdate.cc
+++ BNTrainingUpdate/BNTrainingUpdate.cc
@@ -1,51 +1,7 @@
 #include <iostream>
-#include <algorithm> // for copy
-#include <iterator> // for ostream_iterator
 #include <vector>
-#include <numeric>
 
-#include "acl/acl.h"
-// #include "acl/acl_op.h" // aclopExecuteV2 可以支持动态Shape算子
-#include "acl/acl_op_compiler.h" // aclopCompileAndExecute 只能支持固定Shape算子
-#include "common/logging.h"
-
-#define ACL_CALL(msg) CHECK_EQ(reinterpret_cast<aclError>(msg), ACL_SUCCESS)
-
-class npuTensor {
- public:
-  npuTensor(aclDataType dataType, int numDims, const int64_t *dims, aclFormat format, const float *ptr) {
-    desc = aclCreateTensorDesc(dataType, numDims, dims, format);
-    size = aclGetTensorDescSize(desc);
-    device_ptr = nullptr;
-    ACL_CALL(aclrtMalloc(&device_ptr, size, ACL_MEM_MALLOC_NORMAL_ONLY));
-    if (ptr != nullptr) {
-      ACL_CALL(aclrtMemcpy(device_ptr, size, ptr, size, ACL_MEMCPY_HOST_TO_DEVICE));
-    }
-    buffer =  aclCreateDataBuffer(device_ptr, size);
-  }
-  ~npuTensor() {}
-  void Destroy() {
-    ACL_CALL(aclDestroyDataBuffer(buffer));
-    ACL_CALL(aclrtFree(device_ptr));
-    aclDestroyTensorDesc(desc);
-  }
-  void Print(std::string msg) {
-    size_t numel = size / sizeof(float);
-    std::vector<float> cpu_data(numel, 0);
-    ACL_CALL(aclrtMemcpy(cpu_data.data(), size, device_ptr, size, ACL_MEMCPY_DEVICE_TO_HOST));
-    std::cout << msg << " = [";
-    for (size_t i = 0; i < cpu_data.size(); ++i) {
-      std::cout << cpu_data[i] << ", ";
-    }
-    std::cout << "]" << std::endl;
-  }
-public:
-  size_t size;
-  void * device_ptr;
-  aclTensorDesc* desc;
-  aclDataBuffer* buffer;
-};
-
+#include "common/nputensor.h"
 
 int main() {
   // Init
@@ -62,14 +18,14 @@ int main() {
   const std::string op_type = "BNTrainingUpdate";
 
   // input - x
-  const std::vector<int64_t> x_dims{1, 2, 3, 4};
-  std::vector<float> x_data(24);
+  const std::vector<int64_t> x_dims{1, 2, 4};
+  std::vector<float> x_data(8);
   std::iota(x_data.begin(), x_data.end(), 0);
   // input - sum
   const std::vector<int64_t> c_dims{2};
-  const std::vector<float> sum_data{66.0, 210.0};
+  const std::vector<float> sum_data{6.0, 22.0};
   // input - square_sum
-  const std::vector<float> square_sum_data{506.0, 3818.0};
+  const std::vector<float> square_sum_data{14.0, 126.0};
   // input - scale
   const std::vector<float> scale_data{1.0, 1.0};
   // input - offset
@@ -84,13 +40,13 @@ int main() {
   const float factor = 0.9;
 
   // input - x
-  auto x = new npuTensor(ACL_FLOAT, x_dims.size(), x_dims.data(), ACL_FORMAT_NCHW, x_data.data());
-  auto sum = new npuTensor(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, sum_data.data());
-  auto square_sum = new npuTensor(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, square_sum_data.data());
-  auto scale = new npuTensor(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, scale_data.data());
-  auto offset = new npuTensor(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, offset_data.data());
-  auto mean = new npuTensor(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, mean_data.data());
-  auto var = new npuTensor(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, var_data.data());
+  auto x = new npuTensor<float>(ACL_FLOAT, x_dims.size(), x_dims.data(), ACL_FORMAT_NCHW, x_data.data());
+  auto sum = new npuTensor<float>(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, sum_data.data());
+  auto square_sum = new npuTensor<float>(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, square_sum_data.data());
+  auto scale = new npuTensor<float>(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, scale_data.data());
+  auto offset = new npuTensor<float>(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, offset_data.data());
+  auto mean = new npuTensor<float>(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, mean_data.data());
+  auto var = new npuTensor<float>(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, var_data.data());
 
   // set inputs desc and buffer
   std::vector<aclTensorDesc *> input_descs;
@@ -111,11 +67,11 @@ int main() {
   input_buffers.emplace_back(var->buffer);
 
   // output - y
-  auto y = new npuTensor(ACL_FLOAT, x_dims.size(), x_dims.data(), ACL_FORMAT_NCHW, nullptr);
-  auto mean_out = new npuTensor(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, nullptr);
-  auto var_out = new npuTensor(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, nullptr);
-  auto saved_mean = new npuTensor(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, nullptr);
-  auto saved_var = new npuTensor(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, nullptr);
+  auto y = new npuTensor<float>(ACL_FLOAT, x_dims.size(), x_dims.data(), ACL_FORMAT_NCHW, nullptr);
+  auto mean_out = new npuTensor<float>(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, nullptr);
+  auto var_out = new npuTensor<float>(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, nullptr);
+  auto saved_mean = new npuTensor<float>(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, nullptr);
+  auto saved_var = new npuTensor<float>(ACL_FLOAT, c_dims.size(), c_dims.data(), ACL_FORMAT_ND, nullptr);
 
   // set output desc and buffer
   std::vector<aclTensorDesc *> output_descs;
